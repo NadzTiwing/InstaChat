f44d4743f415518ce29ffcb2ce020166b3e3a961
Nadia Tiwing <tiwingnadia@gmail.com>
2024-01-28 19:55:28 +0800

    add loader and debug user login

--- instachat-app/src/App.tsx
+++ instachat-app/src/App.tsx
@@ -2,15 +2,20 @@ import { auth } from "./firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { ChatRoom } from './pages/chatroom';
 import Login from './pages/login';
+import { Suspense } from "react";
+import Loader from "./pages/chatroom/components/loader";
 
 function App() {
   const [user] = useAuthState(auth);
+
   return (
     <>
-      { !user ? 
+      { !user?.emailVerified ? 
       <Login/>
       : 
-      <ChatRoom/>
+      <Suspense fallback={<Loader />}>
+        <ChatRoom/>
+      </Suspense>
       }
     </>
   )
