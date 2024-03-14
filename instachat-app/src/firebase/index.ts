7e8ad3d3fc5bba3cf9504576befc3670fa88f1e0
Nadia Tiwing <tiwingnadia@gmail.com>
2024-03-14 21:40:10 +0800

    update style

--- instachat-app/src/firebase/index.ts
+++ instachat-app/src/firebase/index.ts
@@ -4,14 +4,6 @@ import { getFirestore } from "firebase/firestore";
 import { IFirebaseConfig } from "../types";
 
 const firebaseConfig: IFirebaseConfig = {
-    // apiKey: "AIzaSyCiMme8y82YgV-4TlrPlrMAA1D86i2nMQA",
-    // authDomain: "chat-db-fa7cd.firebaseapp.com",
-    // databaseURL: "https://chat-db-fa7cd-default-rtdb.asia-southeast1.firebasedatabase.app",
-    // projectId: "chat-db-fa7cd",
-    // storageBucket: "chat-db-fa7cd.appspot.com",
-    // messagingSenderId: "156268552386",
-    // appId: "1:156268552386:web:662e1c44bf91d773ed9b5b",
-    // measurementId: "G-P9FRG67CQX"
     apiKey: import.meta.env.VITE_FIREBASE_API_KEY as string,
     authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN as string,
     databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL as string,
